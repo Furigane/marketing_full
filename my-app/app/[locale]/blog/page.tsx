37605e0bd016d54da14c1d62a71e5da8b5b55176
Egor <test@mail.ru>
2026-05-01 20:00:45 +0400

    add to blog faq block

--- my-app/app/[locale]/blog/page.tsx
+++ my-app/app/[locale]/blog/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/app/components/footer/footer";
 import Header from "@/app/components/headaer/header";
 import { BlogAccordion } from "@/app/components/blog/blog-accordion";
+import PageBottomSections from "@/app/components/common/page-bottom-sections";
 import { TeamSurfaceHeaderSection } from "@/app/components/layout/team-surface-header";
 import { getPublishedBlogPosts } from "@/lib/blog-store";
 
@@ -14,12 +15,13 @@ export default async function BlogPage({
 
   return (
     <>
-      <div className="mx-auto flex w-full min-w-0 max-w-[1280px] flex-col gap-6 px-3 sm:gap-8 sm:px-4 md:px-6 lg:max-w-[1400px]">
+      <main className="mx-auto flex w-full min-w-0 max-w-[1280px] flex-col gap-6 px-3 sm:gap-8 sm:px-4 md:px-6 lg:max-w-[1400px]">
         <TeamSurfaceHeaderSection className="mt-3">
           <Header />
         </TeamSurfaceHeaderSection>
-      </div>
-      <BlogAccordion posts={posts} locale={locale} />
+        <BlogAccordion posts={posts} locale={locale} />
+        <PageBottomSections />
+      </main>
       <Footer />
     </>
   );
